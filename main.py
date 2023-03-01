a65fa80b0315eb45d2b6950c0f63f401856ba902
pa.leveilley <pa.leveilley@groupeonepoint.com>
2023-03-01 22:12:24 +0100

    removed_dead_code

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import cvxopt
 import pickle
 import optimizer
 
-# def main():
 with open("first_database.pkl", "rb") as f:
     x,y = pickle.load(f)
 n= 100
@@ -24,7 +23,7 @@ W2 = np.ones((5,5)) # pour la dual decomposition, W est une matrice d'adjacence 
 W3 = np.array([[1,1,0,0,1],[1,1,1,0,0],[0,1,1,1,0],[0,0,1,1,1],[1,0,0,1,1]]) #cycle
 W4 = np.array([[1,1,0,0,1],[1,1,1,0,0],[0,1,1,1,0],[0,0,1,1,1],[0,0,0,1,1]])
 W5= np.array([[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[0,0,0,0,1]])
-instance_pb = optimizer.instance(x_selected,y_selected,agents,W5,0.5)
+instance_pb = optimizer.instance(x_selected,y_selected,agents,W2,0.5)
 
 # solver_DGD = optimizer.DGD(instance_pb,0.001,1000,7*np.ones(10))
 # solver_DGD.solve()
@@ -34,7 +33,7 @@ instance_pb = optimizer.instance(x_selected,y_selected,agents,W5,0.5)
 # solver_gradient_tracking.solve()
 # print(solver_gradient_tracking.curent_solution)
 
-# solver_dual_decomposition = optimizer.dual_decomposition(instance_pb,0.01,0.01,100,1000,np.ones(10),np.ones(50))
+# solver_dual_decomposition = optimizer.dual_decomposition(instance_pb,0.01,0.01,100,100,np.ones(10),np.ones(50))
 # solver_dual_decomposition.solve()
 # print(solver_dual_decomposition.curent_primal_solution)
 
@@ -42,5 +41,4 @@ solver_dual_decomposition = optimizer.dual_decomposition_edge(instance_pb,0.01,0
 solver_dual_decomposition.solve()
 print(solver_dual_decomposition.curent_primal_solution)
 
-# if __name__ == "__main__":
-#     main()
+
